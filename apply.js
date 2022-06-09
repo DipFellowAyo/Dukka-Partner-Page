c2c7a2e78b7621ce5cc65c040f73cfc153f94af8
DipFellowAyo <103995759+DipFellowAyo@users.noreply.github.com>
2022-06-10 00:53:09 +0100

    new edits

--- apply.js
+++ apply.js
@@ -2,20 +2,56 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    let i;
     let slides = document.getElementsByClassName("mySlide");
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex ++;
     if (slideIndex > slides.length) {
         slideIndex = 1
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 2000);
+    setTimeout(showSlides, 6000);
+}
+
+function validate() {
+
+    if(document.myForm.email.value == "") {
+        alert( "Please provide your Email!" );
+        document.myForm.email.focus() ;
+        return (validateEmail);
+    }
+
+    if(document.myForm.firstname.value == "") {
+        alert( "Please provide your first name!" );
+        document.myForm.firstname.focus() ;
+        return false;
+    }
+
+    if(document.myForm.lastname.value == "") {
+        alert( "Please provide your last name!" );
+        document.myForm.lastname.focus() ;
+        return false;
+    }
+
+    if(document.myForm.phonenumber.value == "" || isNaN(document.myForm.phonenumber.value) || document.myForm.phonenumber.value.length != 11) {
+        alert( "Please provide a phonenumber in the right format!" );
+        document.myForm.phonenumber.focus() ;
+        return false;
+    }
+
+    return true;
+}
+
+function validateEmail() {
+    var emailID = document.myForm.email.value;
+         atpos = emailID.indexOf("@");
+         dotpos = emailID.lastIndexOf(".");
+         
+         if (atpos < 1 || ( dotpos - atpos < 2 )) {
+            alert("Please enter correct email ID")
+            document.myForm.email.focus() ;
+            return false;
+         }
+         return( true );
 }
--- partner.js
+++ partner.js
@@ -1,18 +1,23 @@
-var faq = document.getElementsByClassName("FAQ-header");
+let faq = document.getElementsByClassName("FAQ-header");
+
+let answers = document.getElementsByClassName("FAQ-footer");
 
 for (let i = 0; i < faq.length; i++) {
     faq[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
-        } else {
-            body.style.display = "block";
+        if (parseInt(answers[i].style.height) != answers[i].scrollHeight) {
+            answers[i].style.height = answers[i].scrollHeight + "px";
+            faq[i].classList.toggle("active");
+        }
+        else{
+            answers[i].style.height= "0";
+            faq[i].classList.remove("active");
+        }
+        for (let j = 0; j < answers.length; j++) {
+            if (j !== i) {
+                answers[j].style.height= "0";
+                faq[j].classList.remove("active");
+            }
         }
     });
 }
-
-const hamburger = document.getElementsByClassName("hamburger");
-const nav_menu = document.getElementsByClassName("nav-menu");
 
